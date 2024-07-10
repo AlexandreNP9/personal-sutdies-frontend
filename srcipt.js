d2df70705e7e3651212d67d9963814d3706884f1
Alexandre <alexandre.peder@escola.pr.gov.br>
2024-07-10 11:23:27 -0300

    solicitação com API fetch

--- srcipt.js
+++ srcipt.js
@@ -1,11 +1,29 @@
-//Função para aricionar produtos
-function addProducts() {
-    fetch('https://rms-project-yt.github.io/products/data.json') /*a API fetch é nativa do javascript. Sua função é buscar e enviar dados para ums ervidor web. Responde uma promise, obtida pelo método .then*/
-        .then(response => response.json()) /*transofrma a promise em um JSON*/
-        .then(data => { /*imprime as linhas do JSON*/
-            console.log(data.products);
-        })
-        .catch(error => { /*se tiver algum erro, vai mostrar no terminal*/
-            console.log(error);
-        })
+const URL = 'https://rms-project-yt.github.io/';
+
+//Função de requisição
+async function sendRequest(path, response) {
+    try {
+        const values = await fetch(`${URL}${path}`);
+
+        if (!values.ok) {
+            throw new Error('Erro na solicitação da API');
+        }
+
+        const data = await values.json();
+
+        response(data);
+
+    } catch (error) {
+        console.log(error);
+        alert(`${error}\nNão foi possível obter os dados.`)
+    }
 }
+
+//Função para adicionar produtos
+function addProducts() {
+    sendRequest('products/data.json', (data) => {
+        console.log(data.products);
+    });
+}
+
+addProducts();
