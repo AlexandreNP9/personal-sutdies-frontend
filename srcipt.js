48d00e2da9bd573ebafdc2e6d66640feabcc3174
Alexandre <alexandre.peder@escola.pr.gov.br>
2024-07-10 11:29:44 -0300

    comentários no arquivo js

--- srcipt.js
+++ srcipt.js
@@ -1,19 +1,19 @@
-const URL = 'https://rms-project-yt.github.io/';
+const URL = 'https://rms-project-yt.github.io/'; /*facilita a manutenção futura*/
 
 //Função de requisição
-async function sendRequest(path, response) {
+async function sendRequest(path, response) { /*função criada aqui para poder reutilizar em outras solicitações*/
     try {
-        const values = await fetch(`${URL}${path}`);
+        const values = await fetch(`${URL}${path}`); /*utiliza a constante URL e o parametri path*/ /*a função await indica que as próximas instruções só serão executadas após o fetch ser completado*/
 
-        if (!values.ok) {
+        if (!values.ok) { /*tratamento de erro do fetch*/
             throw new Error('Erro na solicitação da API');
         }
 
         const data = await values.json();
 
-        response(data);
+        response(data); /*a responsta da função é a constante data*/
 
-    } catch (error) {
+    } catch (error) { /*se der algum erro na solicitação*/
         console.log(error);
         alert(`${error}\nNão foi possível obter os dados.`)
     }
@@ -26,4 +26,4 @@ function addProducts() {
     });
 }
 
-addProducts();
+addProducts(); /*assim que abre a página, já é feita a chamada dessa função*/
